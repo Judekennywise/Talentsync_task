ee93ef85540fab39b1ba6fb0d3c0193b9711ac3c
Jude <judeokennywise@gmail.com>
2024-01-22 16:00:20 +0100

    first commit

--- swagger.js
+++ swagger.js
@@ -20,7 +20,7 @@ const options = {
           description: "Local server"
         },
         {
-          url: "<your live url here>",
+          url: "https://blog-post-36hm.onrender.com/docs/",
           description: "Live server"
         },
       ]
